5fa7f4dafe6c78a899a2decdbedb44baa14a115c
StevenCreates <steven@stevencreates.tech>
2021-07-15 17:29:12 -0600

    working on redux

--- src/state/reducers/accountReducer.js
+++ src/state/reducers/accountReducer.js
@@ -1,8 +1,12 @@
-const reducer = (state = 0, action) => {
+const accountReducer = (state = 0, action) => {
     switch (action.type) {
         case "deposit":
             return state + action.payLoad;
         case "withdraw":
-            return state - action.payLoad
+            return state - action.payLoad;
+        defalut:
+            return state
     }
-}
+};
+
+export default accountReducer;
