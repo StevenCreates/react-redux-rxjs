41fd025863f3758bcdcfc4172216c514e6792cca
StevenCreates <steven@stevencreates.tech>
2021-07-15 17:22:09 -0600

    built first reducer

---
+++ src/state/reducers/accountReducer.js
@@ -0,0 +1,8 @@
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case "deposit":
+            return state + action.payLoad;
+        case "withdraw":
+            return state - action.payLoad
+    }
+}
